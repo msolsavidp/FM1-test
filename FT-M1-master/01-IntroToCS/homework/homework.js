7deb8d880e676d1c5cdeee0bfb566c2daf7e26a6
msolsavidp <69515039+msolsavidp@users.noreply.github.com>
2022-08-02 21:51:45 -0300

    DONE

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,13 +1,26 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
 
-}
+  let result = 0;
+  let bin = Array.from(String(num), Number);
+  bin.reverse();
+  for (let i = 0; i<bin.length; i++){
+    let dec = Math.pow(2, i) * bin[i];
+    result = result + dec
+  }
+    return result
+  }
+  
+
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let bin = [];
+  while (num > 0){
+    bin.push(num%2);
+    num=Math.floor(num/2);
+  }
+  return bin.reverse().join('');
 }
 
 
